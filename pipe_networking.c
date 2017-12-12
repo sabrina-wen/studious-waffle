605ba485dc37b2f19c6d0bc7e769103f1c39df80
sabrina-wen <sabrinawn253@gmail.com>
2017-12-12 15:30:28 -0500

    started

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,7 +5,7 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
@@ -19,7 +19,7 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
