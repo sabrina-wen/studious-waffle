64cac21d4b244df75cd30530caaefaac2674c6f2
Kristin Lin <klin11@stuy.edu>
2017-12-16 01:34:08 -0500

    commented

--- basic_server.c
+++ basic_server.c
@@ -1,20 +1,13 @@
 #include "pipe_networking.h"
 
+
+//modifies client text by replacing certain vowels with capitalized letters
 void modify(char * str) {
   char * delim;
-  while (delim = strchr(str, 'i')) {
-    *delim = 'Y';
-  }
-  while (delim = strchr(str, 'e')) {
-    *delim = 'E';
-  }
-  while (delim = strchr(str, 'a')) {
-    *delim = 'A';
-  }
-  while (delim = strchr(str, 'o')) {
-    *delim = 'O';
-  }
-
+  while (delim = strchr(str, 'i')) { *delim = 'Y'; }
+  while (delim = strchr(str, 'e')) { *delim = 'E'; }
+  while (delim = strchr(str, 'a')) { *delim = 'A'; }
+  while (delim = strchr(str, 'o')) { *delim = 'O'; }
 }
 
 int main() {
@@ -23,11 +16,15 @@ int main() {
   int from_client;
   char buf[BUFFER_SIZE];
 
+  //outer loop to keep server running
   while (1) {
+    
+    //perform handshake everytime you reconnect to new client
     printf("\n");
     from_client = server_handshake( &to_client);
     printf("================================\n");
 
+    //while loop to continuously read and modify client info before writing
     while(read(from_client, buf, BUFFER_SIZE)) {
       printf("Recieved from client: %s\n", buf);
       modify(buf);
--- client.c
+++ client.c
@@ -5,15 +5,20 @@ int main() {
 
   int to_server;
   int from_server;
+  char input[BUFFER_SIZE];
 
+  //does handshake once
   from_server = client_handshake( &to_server );
   printf("================================\n");
-  char input[BUFFER_SIZE];
+  
   while(1) {
-    //receives input from user
+
+    //receives input from user and removes newline
     printf("WHATCHA THINKIN'?\n");
     fgets(input, BUFFER_SIZE, stdin);
     *strchr(input, '\n') = 0;
+
+    //writes to server, blocks until reads server response
     write(to_server, input, strlen(input)+1);
     read(from_server, input, strlen(input)+1);
     printf("client recieved: %s\n\n", input);
